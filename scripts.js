fa80ce4b0559fa23bc48e1bdc626d538aa51a3c3
Willyb15 <william.steen.bryant@gmail.com>
2016-03-28 10:08:36 -0400

    Starting off working with Angular
    crash course in Angular

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// -------The plain Vanilla Javascript way of databinding
+
+
+
+
+// var myInput = document.getElementById('myInput');
+
+// myInput.addEventListener('keyup', function(){
+// 	// console.log('User hit a key!');
+	
+// 	var userText = myInput.value;
+// 	document.getElementById('inputText').innerHTML = userText;
+
+
+
+// })
+
+
+// ------------The jQuery Way of databinding------------
+// $('#myInput').keyup(function(){
+// 	$('#inputText').html($('#myInput').val());
+// })
