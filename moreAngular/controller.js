9f8f0085b7769b478bb84d9a92af2c6998b8188a
Willyb15 <william.steen.bryant@gmail.com>
2016-03-29 10:03:41 -0400

    Updated Angular
    Added images to object. Added some form validation

--- moreAngular/controller.js
+++ moreAngular/controller.js
@@ -8,62 +8,62 @@ myApp.controller('myController', function($scope){
 			name: "United States",
 			population:322369319,
 			president: "Barack Obama",
-			flagColor: "Red, White, Blue" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/75px-Flag_of_the_United_States.svg.png" 
 
 		},
 		{
 			name: "Canada",
 			population:36048521,
 			president: "Justin Trudeau",
-			flagColor: "Red, White, Maple Leaf" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Flag_of_Canada.svg/75px-Flag_of_Canada.svg.png" 
 		},
 		{
 			name: "Argentina",
 			population:43417000,
 			president: "Mauricio Macri",
-			flagColor: "Blue, White, Yellow Sun" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/75px-Flag_of_Argentina.svg.png" 
 		},
 		{
 			name: "Spain",
 			population:Number(300000000),
 			president: "Mariano Rajoy",
-			flagColor: "Red, Yellow" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/75px-Flag_of_Spain.svg.png" 
 		},
 		{
 			name: "China",
 			population:1376049000,
 			president: "Xi Jinping",
-			flagColor: "Red, Yellow Stars" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/75px-Flag_of_the_People%27s_Republic_of_China.svg.png" 
 		},
 		{
 			name: "Russia",
 			population:144192450,
 			president: "Vladimir Putin",
-			flagColor: "White, Blue, Red" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/75px-Flag_of_Russia.svg.png" 
 		},
 		{
 			name: "South Africa",
 			population:54956900,
 			president: "Jacod Zuma",
-			flagColor: "Blue, Red, Green Blue, Yellow, White" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/75px-Flag_of_South_Africa.svg.png" 
 		},
 		{
 			name: "New Zealand",
 			population: 4596700,
 			president: "John Key",
-			flagColor: "Blue, Red, with mini GB flag" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_New_Zealand.svg/75px-Flag_of_New_Zealand.svg.png" 
 		},
 		{
 			name: "Iceland",
 			population:323002,
 			president: "Olafur Ragnar Grimsson",
-			flagColor: "Blue, Red Cross" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/70px-Flag_of_Iceland.svg.png" 
 		},
 		{
 			name: "Jordan",
 			population:6459000,
 			president: "King Abdullah II of Jordan",
-			flagColor: "Red, Black, White, Green" 
+			flagColor: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Flag_of_Jordan.svg/75px-Flag_of_Jordan.svg.png" 
 		}
 	];
 
@@ -73,10 +73,10 @@ myApp.controller('myController', function($scope){
 				name: $scope.newName,
 				population: $scope.newPop,
 				president: $scope.newPres,
-				flagColor: $scope.newFlag,
+				flagColor: $scope.newFlag
 			}
 			);
-			// $scope.newCountry = '';
+			// $scope.addCountry = '';
 		}
 
 })
